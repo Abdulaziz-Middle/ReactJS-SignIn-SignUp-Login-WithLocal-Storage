e1b6805016aa4ab783a64298a8b727c912d28ffd
Abdulaziz <151828455+Abdulaziz-Middle@users.noreply.github.com>
2023-11-26 19:55:10 +0500

    Add files via upload

---
+++ SignInSignUpLoginWithLocalStorage/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import SignInSignupWithLocalStorage from './SignInSignUp/SignInSignUp';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <SignInSignupWithLocalStorage/>
+);
